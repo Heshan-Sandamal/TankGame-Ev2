1e8a14f153efd3cd1677eb7f3fa42f497d4f7cd4
Heshan-Sandamal <mhsandamal1@gmail.com>
2015-12-18 06:54:40 +0530

    modified game.cs and add another entity classes

---
+++ XNAGame/XNAGame/PlayerDesc/Brick.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace XNAGame.PlayerDesc
+{
+    class Brick
+    {
+        public int LocationX { get; set; }
+        public int LocationY { get; set; }
+        public int health { get; set; }
+    }
+}
